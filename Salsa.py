83d6971016f08c712cfa82e8f9aa45d7ee594332
MaxiToon <116525245+ProgramadorProfesional@users.noreply.github.com>
2024-11-15 16:07:06 -0300

    Add files via upload

---
+++ Salsa.py
@@ -0,0 +1,38 @@
+# Salsa Lib 0.1
+
+def salsa(agus, joaco):
+    if agus== "capo" or "CAPO":
+        print("Capoagus")
+    if joaco== "puto":
+        print("TE CALMASSS!")
+    if joaco == "mascapo":
+        print(" RED DEAD REDEMPTION 2")
+
+def Kira(state):
+    if state == "BIEN":
+        print("California Kiras")
+
+def TeCalmas():
+    tecalmas_var = int(input("Te calmas?!?!??!?!?!?!"))
+    if tecalmas_var == 0:
+        print("Pagame 100 dolas")
+    elif tecalmas_var == 1:
+        print("Muy bien pisho pisho")
+
+def hoi4():
+    print("ANDA A JUGAR HOI4")
+    print("Aprende a crackear el hell of gates pls")
+
+def sumas_dawn(a,b):
+    print(a-b)
+    print("Hecho por los salsas")
+
+def credits():
+    print("Hecho por OSS")
+    print("Inspirado en sauces")
+
+def swimming(person):
+    if person == "agus":
+        print("MUCHOS TROFEOS")
+    if person == "joaco":
+        print("0 TROFEOS")
